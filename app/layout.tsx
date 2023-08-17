d812bb30cbb3a285465894837abd0ddb512145ad
Chormun Lam <chormunlam@yahoo.com.hk>
2023-08-17 18:31:11 +0000

    Update layout.tsx and page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
+  title: "ChorMun | Portfolio",
   description: "Ricardo is a full-stack developer with 8 years of experience.",
 };
 

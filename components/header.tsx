b14774bfdd0b7ff760ce445c9330bfc2fcd5ae49
Chormun Lam <chormunlam@yahoo.com.hk>
2023-08-18 02:55:37 +0000

    stop for a while

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,7 @@ import clsx from "clsx";
 //import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Header() {
- const { activeSection, setActiveSection } =  useState('home');
+
 
   return (
     <header className="z-[999] relative">

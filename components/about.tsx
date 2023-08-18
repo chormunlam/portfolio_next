f01caae045929118cfd4b68e5293f33a7f49be69
Chormun Lam <chormunlam@yahoo.com.hk>
2023-08-18 01:48:54 +0000

    add scroll mt for space between header and position section

--- components/about.tsx
+++ components/about.tsx
@@ -5,10 +5,11 @@ import React from 'react';
 import SectionHeading from './section-heading'
 
 export default function About(){
-    return <motion.section className='mb-28 max-we-[45rem] text-center leading-8 sm:mb-40'
+    return <motion.section className='mb-28 max-we-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
     initial={{opacity:0, y:100}}
     animate={{opacity:1, y:0}}
-    transition={{delay:0.175}}>
+    transition={{delay:0.175}}
+    id='about'>
         <SectionHeading>About me</SectionHeading>
         <p className="mb-3">I've always been drawn to the 
         endless possibilities of technology. Since graduating 

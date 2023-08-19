46fd037b0749a39f011aabed28941da2666d2331
Chormun Lam <chormunlam@yahoo.com.hk>
2023-08-19 02:30:29 +0000

    create email form

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,9 +1,47 @@
-'use server';
+"use server";
 
-re_3K67q15z_NZyENKNhZseKgpVGJg57PAyG
-export const sendEmail=async(formData: FormData)=>{
-    console.log('Running on server');
-    console.log(formData.get('senderEmail'));
-    console.log(formData.get('message'));
+import React from "react";
+import { Resend } from "resend";
+import { validateString, getErrorMessage } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (formData: FormData) => {
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+
+  // simple server-side validation
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  let data;
+  try {
+    data = await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "chormun.lam26@qmail.cuny.edu",
+      subject: "Message from contact form",
+      reply_to: senderEmail,
+      react: React.createElement(ContactFormEmail, {
+        message: message,
+        senderEmail: senderEmail,
+      }),
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
+
+  return {
+    data,
+  };
 };
